3ee148284cfd8551e9d7f73ba1a1ea273e9d19da
shibamoto <silverbirder@gmail.com>
2020-03-22 13:36:37 +0900

    Add angular project

---
+++ angular/src/app/welcome/welcome.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { WelcomeRoutingModule } from './welcome-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    WelcomeRoutingModule,
+  ],
+})
+export class WelcomeModule { }
