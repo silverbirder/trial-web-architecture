928d13604fd904a8ed051f63e19b8ce8e4ec7023
shibamoto <silverbirder@gmail.com>
2020-04-06 21:57:30 +0900

    Trying the requestFragment

--- think/team-page/server.js
+++ think/team-page/server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const Tailor = require('node-tailor');
-const tailor = new Tailor({});
+const filterHeaders = require('node-tailor/lib/request-fragment');
 const port = process.env.PORT || 3000;
+const teamSearchHost = process.env.TEAM_SEARCH_HOST || 'http://localhost:3003/search';
+const teamDecideHost = process.env.TEAM_DECIDE_HOST || 'http://localhost:3002/decide';
+
+
+const tailor = new Tailor({
+    requestFragment: (async(a, url, attributes, req)=> {
+        attributes.src = teamDecideHost;
+        return await filterHeaders(url, attributes, req);
+    })
+});
 
 const app = express();
 app.use('/static/', express.static('dist'));
