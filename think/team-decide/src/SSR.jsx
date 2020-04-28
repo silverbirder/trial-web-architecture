88e19e4443703db929f3be4b0d1d8695da8b2c22
shibamoto <silverbirder@gmail.com>
2020-04-28 20:06:21 +0900

    Add item components into SSR

--- think/team-decide/src/SSR.jsx
+++ think/team-decide/src/SSR.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Items from "./components/Items";
 
 const SSR = (props) => {
     return (
         <div id="team-decide">
-            <div id="team-decide-items"></div>
+            <div id="team-decide-items">
+                <Items {...{items: props.items}} />
+            </div>
         </div>
     )
 };
